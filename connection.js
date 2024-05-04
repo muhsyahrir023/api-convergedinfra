60fb1f6e8123712c0f47d55f3f2e4f48f824711e
muhsyahrir023 <muh.syahrir@mastersystem.co.id>
2024-05-04 11:25:06 +0700

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const database = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "convergedinfra"
+     host: "sql.freedb.tech",
+    user: "freedb_cidev",
+    password: "$33Q!TpYfaSe9gz",
+    database: "freedb_convergedinfra"
 });
 
 database.connect((err) => {
